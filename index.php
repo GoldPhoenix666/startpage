56e3a98f36996a01310a9cec6ed76cde1e873aff
GoldPhoenix666 <32621035+GoldPhoenix666@users.noreply.github.com>
2017-10-09 13:50:00 +0100

    Added files
    first opening

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+	<title>StartPage</title>
+
+
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+
+<div id="devel">
+	
+<ul style="color:lightblue;" 
+>Local Dev
+<br />
+	<li id="develtext">
+		<a href="https://github.com/GoldPhoenix666/messaround">Github</a>
+	</li>
+<br />
+	<li id="develtext">
+		<a href="http://localhost/messaround/correct.php">Localhost - Correct</a>
+	</li>
+<br />
+	<li id="develtext">
+		<a href="http://localhost/phpmyadmin">PhpMyAdmin</a>
+	</li>
+
+</ul>
+
+<!--<ul>Youtube
+<br />
+	<li>
+		<a href="https://www.youtube.com/playlist?list=PL7BhAa77vqwv04dAo106EPoB8c_mZJyB_">Music Playlist 7</a>
+	</li>
+<br />
+	<li>
+		<a href="https://www.youtube.com/feed/subscriptions">Subscriptions</a>
+	</li>
+</ul>-->	
+
+
+
+<ul style="color:red;">External Info
+<br />	
+	<li id="exttext" >
+		<a href="https://www.reddit.com/r/webdev/">/r/WebDev</a>
+	</li>
+<br />
+	<li id="exttext" >
+		<a href="https://www.reddit.com/r/web_design/">/r/WebDesign</a>
+	</li>
+<br />
+	<li id="exttext" >
+		<a href="https://stackoverflow.com/">Stack Overflow</a>
+	</li>
+
+
+</ul>
+
+<ul style="color:lightgreen;">Useful Links
+	<br />
+	<li id="usetext">
+		<a href="https://www.google.co.uk">Google</a>
+	</li>
+</ul>
+
+
+<ul id="hiddentext" style="color:black;">Hidden Links
+<br />
+	<li id="hiddentext">
+		<a href="https://www.facebook.com">Facebook</a>
+	</li>
+<br />
+	<li id="hiddentext">
+		<a href="https://www.youtube.com/playlist?list=PL7BhAa77vqwv04dAo106EPoB8c_mZJyB_">Music Playlist 7</a>
+	</li>
+<br />
+	<li id="hiddentext">
+		<a href="https://www.youtube.com/feed/subscriptions">Subscriptions</a>
+	</li>
+</ul>
+
+<ul>
+<br />
+	<li>
+		<a href=""></a>
+	</li>
+<br />	
+	<li>
+		<a href=""></a>
+	</li>
+
+</ul>
+
+
+
+</div>	
+
+
+</body>
+</html>
